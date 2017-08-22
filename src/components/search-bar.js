65b3ad68e2483d706a1494ef1afa8db786a26e6e
jordanpurinton <jordanomac@gmail.com>
2017-08-21 21:15:03 -0600

    make call to youtube search API and log data to console

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -1,8 +1,19 @@
 import React, {Component} from 'react';
 
 class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {searchText: ''};
+    }
+
     render() {
-        return <input onChange={event => console.log(event.target.value)} />;
+        return (
+        <div>
+        <input
+        value={this.state.searchText}
+        onChange={event => this.setState({searchText: event.target.value})} />
+        </div>
+    )
     }
 
 }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search-bar';
+import YTSearch from 'youtube-api-search';
 
 const API_KEY = 'AIzaSyCWF65gNA1kfQwBKdvIrgijeFd0zLfNcKs';
+
+YTSearch({key: API_KEY, searchText: 'memes'}, function (data) {
+    console.log(data);
+});
 
 const App = () => {
     return (
