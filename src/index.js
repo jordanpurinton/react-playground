9235f2e108f74fd52275380c8f0ff217164076b9
jordanpurinton <jordanomac@gmail.com>
2017-08-21 20:52:30 -0600

    update search bar component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 // react intro
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search-bar';
 
 const API_KEY = 'AIzaSyCWF65gNA1kfQwBKdvIrgijeFd0zLfNcKs';
 
 const App = () => {
-    return <div>Hi!</div>;
+    return (
+    <div>
+        <SearchBar/>
+    </div>
+    );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
