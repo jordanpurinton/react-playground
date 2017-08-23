72aca64d38d2010f4d3628071a0994188746ec54
jordanpurinton <jordanomac@gmail.com>
2017-08-22 18:35:32 -0600

    populate video-list w/ list items consiting of thumbnail and heading

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ class App extends Component {
         return (
             <div>
                 <SearchBar/>
-                <VideoList videos="{this.state.videos}"/>
+                <VideoList videos={this.state.videos}/>
             </div>
         );
     }
