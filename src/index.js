cc5acdb8f280c44f900abfd9a924e4ae9fb6cff0
jordanpurinton <jordanomac@gmail.com>
2017-08-22 18:56:14 -0600

    user can click on a video, updating app state and setting current video

--- src/index.js
+++ src/index.js
@@ -10,21 +10,28 @@ const API_KEY = 'AIzaSyCWF65gNA1kfQwBKdvIrgijeFd0zLfNcKs';
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = {videos: []};
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
 
         YTSearch({key: API_KEY, searchText: 'meme'},
             (videos) => {
-                this.setState({videos}); // es6 translation => this.setState({videos: videos})
+                this.setState({
+                    videos: videos,  // es6 translation => this.setState({videos: videos})
+                    selectedVideo: videos[0]
+                });
             });
-
     }
 
     render() {
         return (
             <div>
                 <SearchBar/>
-                <VideoDetail video={this.state.videos[0]}/>
-                <VideoList videos={this.state.videos}/>
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos}/>
             </div>
         );
     }
